933bd07c0f44cf5f125b7c8c4a2634c7f1b3cb9c
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-16 22:27:05 -0300

    second-commit

---
+++ containers/Login.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
+
+export const Login = () => {
+    return (
+        <div className="container-login">
+            <img src="logo.svg" alt="Logo FIAP" className="logo"/>
+            <div className="form">
+                <div className="input">
+                    <img src="mail.svg" alt="Login"/>
+                    <input placeholder="Login"/>
+                </div>
+
+                <div className="input">
+                    <img src="lock.svg" alt="Senha"/>
+                    <input placeholder="Senha"/>
+                </div>
+                <button>Entrar</button>
+            </div>
+        </div>
+    );
+}
